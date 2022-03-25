ec8986d42aef4530c39663c9adb4231ecd39006a
Apple <jevesh@clinikk.com>
2022-03-25 12:31:19 +0530

    Airdropping SOL function

--- index.js
+++ index.js
@@ -27,3 +27,18 @@ const getWalletBalance = async () => {
     console.log(err);
   }
 };
+
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+    console.log('-- Airdropping 2 SOL --');
+    const fromAirDropSignature = await connection.requestAirdrop(
+      new PublicKey(walletKeyPair.publicKey),
+      2 * LAMPORTS_PER_SOL,
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (err) {
+    console.log(err);
+  }
+};
